d95f43688c20924f16be9d07d53fdc990c48e2b2
Thomas MIllard <56883886+ThomasMillard123@users.noreply.github.com>
2022-12-28 19:55:27 +0000

    update page work from json

--- assets/js/ContentLoad.js
+++ assets/js/ContentLoad.js
@@ -1,7 +1,7 @@
 async function populate() {
 
-    //const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-    const requestURL = 'https://ThomasMillard123.github.io/assets/js/content.json';
+    
+    const requestURL = 'https://ThomasMillard123.github.io/assets/content.json';
    
     const request = new Request(requestURL);
 
@@ -12,7 +12,7 @@ async function populate() {
 
     const superHeroes = JSON.parse(superHeroesText);
     populateHeader(superHeroes);
-    //populateHeroes(superHeroes);
+    populateHeroes(superHeroes);
 
 }
 function populateHeader(obj) {
@@ -27,34 +27,55 @@ function populateHeader(obj) {
 }
 
 function populateHeroes(obj) {
-    const section = document.querySelector('section');
-    const heroes = obj.members;
+    const section = document.querySelector('#current_Work');
+    const heroes = obj.Content;
 
     for (const hero of heroes) {
-        const myArticle = document.createElement('article');
+        const myArticle = document.createElement('section');
         const myH2 = document.createElement('h2');
-        const myPara1 = document.createElement('p');
-        const myPara2 = document.createElement('p');
-        const myPara3 = document.createElement('p');
-        const myList = document.createElement('ul');
 
+        //img
+        const span1 = document.createElement('span');
+        const img1 = document.createElement('img');
+        span1.className = "image main";
+        span1.appendChild(img1);
+
+        //des
+        const Desc1 = document.createElement('p');
+        Desc1.textContent = hero.Description1;
+        const Desc2 = document.createElement('p');
+        Desc2.textContent = hero.Description2;
+        const Desc3 = document.createElement('p');
+        Desc3.textContent = hero.Description3;
         myH2.textContent = hero.name;
-        myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
-        myPara2.textContent = `Age: ${hero.age}`;
-        myPara3.textContent = 'Superpowers:';
-
-        const superPowers = hero.powers;
-        for (const power of superPowers) {
+        img1.src = `images/${hero.HeadImg}`;
+       
+        //things
+        const myList = document.createElement('ul');
+        const List = hero.Featuers;
+        for (const itmes of List) {
             const listItem = document.createElement('li');
-            listItem.textContent = power;
+            listItem.textContent = itmes;
             myList.appendChild(listItem);
         }
 
+        //tags
+        const tag = document.createElement('p');
+        const List2 = hero.Tags;
+        for (const itmes of List2) {
+            tag.textContent += "|"+ itmes;
+           
+        }
+        tag.textContent += "|";
+
         myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
+        myArticle.appendChild(span1);
+        myArticle.appendChild(Desc1);
         myArticle.appendChild(myList);
+        myArticle.appendChild(Desc2);
+        myArticle.appendChild(Desc3);
+        myArticle.appendChild(tag);
+
 
         section.appendChild(myArticle);
     }
